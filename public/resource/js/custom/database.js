578cd2900d71dbec0b751b9380710666902bdab9
itsmeashu2601 <soulsoft.123ashutosh@gmail.com>
2024-01-16 15:14:57 +0530

    [base_url]

--- public/resource/js/custom/database.js
+++ public/resource/js/custom/database.js
@@ -1,33 +1,28 @@
 $("#databaseForm").on("submit", function (e) {
     e.preventDefault();
+    
+    var base_url = "https://dynamicdb.soulsoftsks.in/";
     var databaseNameValue = $('#databaseName').val();
     var userName = $('#userName').val();
     var userPassword = $('#userPassword').val();
 
     $.ajax({
-        url: "createDatabase/" + databaseNameValue + "/" + userName + "/" + userPassword,
+        url: base_url + "createDatabase/" + databaseNameValue + "/" + userName + "/" + userPassword,
         type: "POST",
-
-        cache: false,
-
-        contentType: false,
-
-        processData: false,
-
         dataType: "json",
-
         success: function (response) {
-            if (response.status == 200) {
-                swal("Good job!", response.message, "success");
+            if (response.status === 200) {
+                swal("Success", response.message, "success");
                 setTimeout(function () {
-                    $(location).attr("href", "/");
+                    window.location.href = base_url;
                 }, 2000);
             } else {
-                swal("Good job!", response.message, "success");
-                setTimeout(function () {
-                    $(location).attr("href", "/");
-                }, 2000);
+                swal("Error", response.message, "error");
             }
         },
+        error: function (xhr, status, error) {
+            swal("Error", "An error occurred while processing your request.", "error");
+            console.error(xhr.responseText);
+        }
     });
 });
